2c9106bde4f5efc25b56b67803d75668d531953c
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-06-08 21:02:00 +0330

    schtasks On Startup.bat

--- Schedule Task/schtasks On Startup.bat
+++ Schedule Task/schtasks On Startup.bat
@@ -1,2 +1,17 @@
 #To complement your weekly reboot, you could schedule tasks to launch specific programs on startup: 
 schtasks /create /sc onstart /tn "launch Chrome on startup" /tr "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" 
+
+# /sc is Specifies the schedule type.
+      #MINUTE
+      #HOURLY
+      #DAILY
+      #WEEKLY
+      #MONTHLY
+      #ONCE
+      #ONSTART
+      #ONLOGON
+      #ONIDLE
+         
+ #/tn <taskname>   ""
+  
+ #/tr <Taskrun>    ""
