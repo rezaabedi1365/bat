c2d132fc92fec017aa108500e8915387bfbab300
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-06-08 20:57:38 +0330

    Update schtasks Weekly.bat

--- Schedule Task/schtasks Weekly.bat
+++ Schedule Task/schtasks Weekly.bat
@@ -1,2 +1,27 @@
 #For example, you could schedule your computer to reboot at 11pm every Friday: 
 schtasks /create /sc weekly /d FRI /tn "auto reboot computer weekly" /st 23:00 /tr "shutdown -r -f -t 10" 
+
+# /sc is Specifies the schedule type.
+      #MINUTE
+      #HOURLY
+      #DAILY
+      #WEEKLY
+      #MONTHLY
+      #ONCE
+      #ONSTART
+      #ONLOGON
+      #ONIDLE
+# /d  is  DAYs.
+      #value between 1-52 weeks 
+      #OR
+      #Specefic Days
+         #SAT
+         #SUN
+         #MON
+         #TUE
+         #WED
+         #THU
+         #FRI
+ #/tn <taskname>     
+         
+      
