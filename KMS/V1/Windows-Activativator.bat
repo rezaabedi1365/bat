cd90ef3a52414635b3cd92f4458b0b49692b13bb
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-05 12:29:03 +0330

    Add files via upload

---
+++ KMS/V1/Windows-Activativator.bat
@@ -0,0 +1,23 @@
+@echo off
+title Windows Activator - Faradis Alborz
+call :isAdmin
+if %errorlevel% == 0 (
+goto :run
+) else (
+echo Requesting administrative privileges...
+goto :UACPrompt
+)
+exit /b
+:isAdmin
+fsutil dirty query %systemdrive% >nul
+exit /b
+:run
+slmgr /skms activator.faradis.net
+slmgr /ato
+exit /b
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B`
