57a359c4d86fa4be770b223c3de3225f96902a3d
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 16:29:35 +0330

    Update reg add.bat

--- Registry/reg add.bat
+++ Registry/reg add.bat
@@ -1,1 +1,5 @@
+#https://www.windowscentral.com/how-edit-registry-using-command-prompt-windows-10
+#/t Type: Specifies the type of registry entries. Here's the list of valid types: REG_SZ, REG_MULTI_SZ, REG_DWORD_BIG_ENDIAN, REG_DWORD, REG_BINARY, REG_DWORD_LITTLE_ENDIAN, REG_LINK, REG_FULL_RESOURCE_DESCRIPTOR, and REG_EXPAND_SZ.
+
+
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 4458 /f 
