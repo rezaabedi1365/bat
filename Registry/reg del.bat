7938b8d78a700cd3dd1d84780dadce2107c217b9
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 15:56:50 +0330

    Update reg del.bat

--- Registry/reg del.bat
+++ Registry/reg del.bat
@@ -2,6 +2,7 @@
 
 #reg delete  Registry_key_path /v Registry_value_name /f
 #reg delete  Registry_key_path /f
+#If the registry key has space in its name, you can use double quotes as below.
+#reg delete  "Registry_key_path" /v Registry_value_name /f
 
-
-reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe\
+reg delete  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe\"
