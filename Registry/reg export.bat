5491507b0355e87179708f98f7d297358fd78959
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 17:23:14 +0330

    Update reg export.bat

--- Registry/reg export.bat
+++ Registry/reg export.bat
@@ -1,8 +1,11 @@
+# HKEY_CLASSES_ROOT=HKCR , HKEY_CURRENT_USER=HKCU , HKEY_LOCAL_MACHINE=HKLM  , HKEY_USERS=HKU , HKEY_CURRENT_CONFIG=HKCC 
+
+
 @echo off
 
 #reg export [registry path] [path to store backup] /y
 
-#reg export "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" c:\KMS_Registry_backup.reg /y
+reg export "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" c:\KMS_Registry_backup.reg /y
  
 reg export HKCR C:RegBackHKCR.Reg /y
 
