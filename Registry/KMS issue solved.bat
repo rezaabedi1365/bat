4ca6d4b9c1f59bae554cdf38ca99e156a227b72a
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 17:15:49 +0330

    Create KMS issue solved.bat

---
+++ Registry/KMS issue solved.bat
@@ -0,0 +1,12 @@
+@echo off
+
+reg export  "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" c:\KMS_Registry_backup.reg /y
+reg delete  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" /f
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\55c92734-d682-4d71-983e-d6ec3f16059f\ef6cfc9f-8c5d-44ac-9aad-de6a2ea0ae03" /v KeyManagementServiceName /t REG_SZ /d 172.16.1.16 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\55c92734-d682-4d71-983e-d6ec3f16059f\ef6cfc9f-8c5d-44ac-9aad-de6a2ea0ae03" /v KeyManagementServicePort /t REG_SZ /d 1688 /f 
+
+slmgr /skms 172.16.1.16
+slmgr /ato
+
+pause
