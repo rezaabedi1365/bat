9845a090ab48843286e16f0ef3c96297517dfbdf
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 14:45:16 +0330

    Create reg del.bat

---
+++ Registry/reg del.bat
@@ -0,0 +1,7 @@
+
+
+#reg delete  Registry_key_path /v Registry_value_name /f
+#reg delete  Registry_key_path /f
+
+
+reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe\
