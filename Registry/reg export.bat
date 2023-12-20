fa83e2c11ceba43e138d96494a714e7310b94157
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-20 14:29:11 +0330

    Create reg export.bat

---
+++ Registry/reg export.bat
@@ -0,0 +1,15 @@
+@echo off
+
+#reg export [registry path] [path to store backup] /y
+
+#reg export HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe c:\KMS_Registry_backup.reg /y
+ 
+reg export HKCR C:RegBackHKCR.Reg /y
+
+reg export HKCU C:RegBackHKCU.Reg /y
+
+reg export HKLM C:RegBackHKLM.Reg /y
+
+reg export HKU C:RegBackHKU.Reg /y
+
+reg export HKCC C:RegBackHKCC.Reg /y
