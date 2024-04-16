cd5f0b7b8a32b1cbb4bc977a4240fc6e7bf78f0a
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-04-16 15:47:06 +0330

    Update Change RDP Port.bat

--- Registry/Change RDP Port.bat
+++ Registry/Change RDP Port.bat
@@ -2,3 +2,9 @@
 set /p RDP= what port do you want set for Remote Desktop?
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d %RDP% /f 
 pause
+
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d faratest /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d conference /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d 123456aA /f
