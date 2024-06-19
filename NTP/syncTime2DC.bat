bff8f4ba9c225f051c3fd72bfa82ad690cd191e9
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-06-19 19:19:40 +0330

    Update syncTime2DC.bat

--- NTP/syncTime2DC.bat
+++ NTP/syncTime2DC.bat
@@ -1,3 +1,20 @@
+
+Computer> Configuration> Policies> Administrative Templates> System > Windows Time Service > Time Providers
+        > Configure Windows NTP Client
+        > Enable Windows NTP Client
+        > Enable Windows NTP Server
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer" /v Enabled /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers " /v 1 /t REG_SZ /d 10.10.12.18 /f 
+
+#reconfig 
+      w32tm /config /update
+
+verify ntp server:
+  w32tm /query /status
+  w32tm /query /peers 
+  w32tm /query /configuration
+
 @echo off
 w32tm /config /syncfromflags:DOMHIER /reliable:YES /update
 w32tm /resync /nowait
