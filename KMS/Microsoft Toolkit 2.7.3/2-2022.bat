317bcc9ffdb2e6ebf4ac088a259d1526ce18094f
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-07-23 18:05:55 +0330

    Add files via upload

---
+++ KMS/Microsoft Toolkit 2.7.3/2-2022.bat
@@ -0,0 +1,3 @@
+slmgr /ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+slmgr /skms 172.16.81.163
+slmgr /ato
---
+++ KMS/Microsoft Toolkit 2.7.3/KMS Issue solved.bat
@@ -0,0 +1,14 @@
+
+@echo off
+
+reg export  "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" c:\KMS_Registry_backup.reg /y
+reg delete  "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SppExtComObj.exe" /f
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\55c92734-d682-4d71-983e-d6ec3f16059f\ef6cfc9f-8c5d-44ac-9aad-de6a2ea0ae03" /v KeyManagementServiceName /t REG_SZ /d 172.16.81.163 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\55c92734-d682-4d71-983e-d6ec3f16059f\ef6cfc9f-8c5d-44ac-9aad-de6a2ea0ae03" /v KeyManagementServicePort /t REG_SZ /d 1688 /f 
+
+slmgr /skms 172.16.81.163
+slmgr /ato
+
+pause
+
